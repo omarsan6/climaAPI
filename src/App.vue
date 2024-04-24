239cb383c188f734b959facb35fe5aa37806f45d
omarsanchez <omarcore@gmail.com>
2024-04-23 19:52:49 -0600

    Creando el formulario y la alerta

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+  import Formulario from './components/Formulario.vue'
 </script>
 
 <template>
-  
+
+  <h1 class="titulo">Buscador de clima</h1>
+
+  <div class="contenedor buscador-clima">
+    <Formulario/>
+  </div>
+
 </template>
 
 <style scoped>
