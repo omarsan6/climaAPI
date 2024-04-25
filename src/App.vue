d4ceec75b4a80264f19ffa49caab7a407f57ca80
omarsanchez <omarcore@gmail.com>
2024-04-25 12:27:07 -0600

    Mostrando el clima con spiner

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
   import Formulario from './components/Formulario.vue'
+  import Clima from './components/Clima.vue'
+  import Spiner from './components/Spiner.vue'
+  import Alerta from './components/Alerta.vue'
   import useClima from './composables/useClima.js'
 
-  const {obtenerClima} = useClima()
+  const {obtenerClima, clima,mostrarClima,cargando,error} = useClima()
 
 
 
@@ -15,6 +18,14 @@
   <div class="contenedor buscador-clima">
     <Formulario
       @obtener-clima="obtenerClima"/>
+
+    <Alerta v-if="error">{{ error }}</Alerta>
+    
+    <Spiner v-if="cargando"/>
+
+    <Clima 
+      v-if="mostrarClima" 
+      :clima="clima"/>
   </div>
 
 </template>
